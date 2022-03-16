0baada3e5fb5178b1dd02772c0295300a4aa1d08
ldh3907 <ldh165163@gmail.com>
2022-03-16 11:35:17 +0900

    init

---
+++ src/kr/hs/dgsw/java/dept21/Hello.java
@@ -0,0 +1,14 @@
+package kr.hs.dgsw.java.dept21;
+
+public class Hello {
+	
+	public void sayHello() {
+		System.out.println("hello world");
+	}
+	
+	public void sayHello(String name) {
+		System.out.println("hello" + name + "!");
+	}
+	
+	
+}
